358a4143063d93d447a0e1b67a337e469f4cc08b
moranpol <moranpol3@gmail.com>
2024-05-07 12:38:42 +0300

    edit moranpol

--- MoranPol.java
+++ MoranPol.java
@@ -1,7 +1,7 @@
 public class MoranPol{
 	public static void main(String[] args){
 		for(int i = 1; i <= 10; i++){
-			System.out.println("Moran Pol..." + i);
+			System.out.println("Moran Pol :)..." + i);
 		}
 	}
 }
